8051ac0151f2d2a3e8d04c028875c21d257736fd
mnflwr-ee <desereemagkasi6@gmail.com>
2025-05-30 08:44:34 +0800

    address fixed

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -27,7 +27,7 @@ class employeecontroller extends Controller
             'lname' => 'required|max:255|string',
             'midname' => 'required|max:255|string',
             'age' => 'required|integer',
-            'address' => 'required|integer',
+            'address' => 'required|max:255|string',
             'zip' => 'required|integer',    
         ]);
 
